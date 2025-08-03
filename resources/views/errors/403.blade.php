0f7895986edfaf9c4155a4b79e2b0e0999d7d810
ViniciusJ <viniciusjose9@outlook.com>
2025-08-03 16:00:08 -0300

    fix: adapting error screens

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,11 +1,7 @@
-@extends('layouts.app')
-
-@section('title', 'Acesso Negado')
-
-@section('content')
+<x-app-layout>
     <div class="text-center mt-20">
-        <h1 class="text-5xl font-bold text-red-600">403</h1>
-        <p class="text-xl mt-4">Você não tem permissão para acessar esta página.</p>
-        <a href="{{ url()->previous() }}" class="text-blue-500 underline mt-6 inline-block">Voltar</a>
+        <h1 class="text-6xl font-bold text-red-600">403</h1>
+        <p class="text-xl mt-4 text-gray-800 dark:text-gray-200">Você não tem permissão para acessar esta página.</p>
+        <a href="{{ url()->previous() }}" class="mt-6 inline-block text-blue-600 hover:underline">Voltar</a>
     </div>
-@endsection
+</x-app-layout>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,11 +1,7 @@
-@extends('layouts.app')
-
-@section('title', 'Erro interno')
-
-@section('content')
+<x-app-layout>
     <div class="text-center mt-20">
         <h1 class="text-5xl font-bold text-red-700">500</h1>
         <p class="text-xl mt-4">Ocorreu um erro interno. Tente novamente mais tarde.</p>
         <a href="{{ url('/') }}" class="text-blue-500 underline mt-6 inline-block">Ir para página inicial</a>
     </div>
-@endsection
+</x-app-layout>
